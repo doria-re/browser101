7333cc6f3656005dd4ac0c2145a8091d34e923ea
doria <resonanta0@gmail.com>
2022-02-15 12:13:06 +0900

    change computer

---
+++ 2/app.js
@@ -0,0 +1,58 @@
+/*
+function clickedSquareElement(element){
+  console.log(element.getBoundingClientRect());
+}
+
+function clickedSquareEvent(event){
+  console.log
+(`client X: ${event.clientX}
+client Y: ${event.clientY}
+page X: ${event.pageX}
+page Y: ${event.pageY}`)
+}
+
+
+/*
+하나만 되면 되는건데 오해함 다해야되는줄...
+하나만 하면 된다면 어떻게 할까?
+
+
+*/
+const one = document.querySelector("#one");
+
+
+function clicked (event){
+  const oneRect = one.getBoundingClientRect();
+  console.log(oneRect);
+  console.log
+(`client X: ${event.clientX}
+client Y: ${event.clientY}
+page X: ${event.pageX}
+page Y: ${event.pageY}`)
+}
+
+one.addEventListener("click", clicked);
+
+
+const $scrollBy = document.querySelector("#scroll-by");
+const $scrollTo = document.querySelector("#scroll-to");
+const $scrollInto = document.querySelector("#scroll-into");
+
+
+
+$scrollBy.addEventListener("click", () => {scrollBy(0, 100)} );
+$scrollTo.addEventListener("click", () => {scroll(0, 100)});
+$scrollInto.addEventListener("click", () => {one.scrollIntoView()});
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ coordinates/main.js
@@ -0,0 +1,27 @@
+const $target = document.querySelector("#target");
+const $xLine = document.querySelector("#x-line");
+const $yLine = document.querySelector("#y-line");
+const $pxs = document.querySelector("#pxs");
+const $xPx = document.querySelector("#x-px");
+const $yPx = document.querySelector("#y-px");
+
+console.dir($xPx)
+
+function onMouseMove(event){
+  const mouseX = event.clientX;
+  const mouseY = event.clientY;
+  $target.style.left = `${mouseX}px`;
+  $target.style.top = `${mouseY}px`;
+  $xLine.style.top = `${mouseY-9}px`;
+  $yLine.style.left = `${mouseX-1}px`;
+  $pxs.style.left = `${mouseX+20}px`
+  $pxs.style.top = `${mouseY+20}px`
+  $xPx.innerText = `${mouseX}px`
+  $yPx.innerText = `${mouseY}px`
+}
+
+/* document.addEventListener("mousemove", event => {
+
+})
+이렇게 구현하면 div로 전체 감쌀 필요 없이 구현 가능 
+*/
