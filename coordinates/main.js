86ae9119b23a46465f2a32107f831ff47179e78e
doria <resonanta0@gmail.com>
2022-02-27 15:46:37 +0900

    coordinates 성능개선 - top, left -> transform: translate()로 변경 - innerText -> textContent로 변경

--- coordinates/main.js
+++ coordinates/main.js
@@ -2,18 +2,14 @@ const $target = document.querySelector("#target");
 const $xLine = document.querySelector("#x-line");
 const $yLine = document.querySelector("#y-line");
 const $pxs = document.querySelector("#pxs");
-const $xPx = document.querySelector("#x-px");
-const $yPx = document.querySelector("#y-px");
 
 document.addEventListener("mousemove", event =>{
+  console.log(event)
   const mouseX = event.clientX;
   const mouseY = event.clientY;
-  $target.style.left = `${mouseX}px`;
-  $target.style.top = `${mouseY}px`;
-  $xLine.style.top = `${mouseY-9}px`;
-  $yLine.style.left = `${mouseX-1}px`;
-  $pxs.style.left = `${mouseX+20}px`
-  $pxs.style.top = `${mouseY+20}px`
-  $xPx.innerText = `${mouseX}px`
-  $yPx.innerText = `${mouseY}px`
+  $target.style.transform = `translate(${mouseX-60}px, ${mouseY-60}px)`;
+  $xLine.style.transform = `translateY(${mouseY-9}px)`;
+  $yLine.style.transform = `translateX(${mouseX-1}px)`;
+  $pxs.style.transform = `translate(${mouseX+30}px, ${mouseY+30}px`
+  $pxs.textContent = `${mouseX}px ${mouseY}px`
 })
